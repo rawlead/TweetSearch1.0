29495ed53d6a598f48b2284e9455e25010895931
Ivan <rawlead@gmail.com>
2017-03-04 01:54:30 +0100

    Initial commit

---
+++ src/main/java/com/ivanshyrai/MasterprojectApplication.java
@@ -0,0 +1,12 @@
+package com.ivanshyrai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MasterprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MasterprojectApplication.class, args);
+	}
+}
