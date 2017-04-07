b48307f8f8035efaba87c811ca420a6da3942879
Ivan <rawlead@gmail.com>
2017-04-07 20:41:59 +0200

    add session builder test class

---
+++ src/test/java/com/ivanshyrai/controller/HomeControllerTest.java
@@ -0,0 +1,71 @@
+package com.ivanshyrai.controller;
+
+import com.ivanshyrai.SessionBuilder;
+import com.ivanshyrai.profile.UserProfileSession;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mock.web.MockHttpSession;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import java.util.Arrays;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest
+@WebAppConfiguration
+@ActiveProfiles("redis")
+public class HomeControllerTest {
+
+    @Autowired
+    private WebApplicationContext wac;
+    private MockMvc mockMvc;
+
+    @Before
+    public void setup() {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(this.wac).build();
+    }
+
+    @Test
+    public void should_redirect_to_profile() throws Exception {
+        this.mockMvc.perform(get("/"))
+                .andDo(print())
+                .andExpect(status().isFound())
+                .andExpect(redirectedUrl("/profile"));
+    }
+
+    // custom sessionBuilder class
+    @Test
+    public void should_redirect_to_tastes() throws Exception {
+        MockHttpSession session = new SessionBuilder().userTastes("spring",
+                "groovy").build();
+        this.mockMvc.perform(get("/")
+                .session(session))
+                .andExpect(status().isFound())
+                .andExpect(redirectedUrl("/search/mixed;keywords=spring,groovy"));
+    }
+
+    //default mock http session
+    //    @Test
+//    public void should_redirect_to_tastes() throws Exception {
+//        MockHttpSession session = new MockHttpSession();
+//        UserProfileSession sessionBean = new UserProfileSession();
+//        sessionBean.setTastes(Arrays.asList("spring","groovy"));
+//        session.setAttribute("scopedTarget.userProfileSession",sessionBean);
+//        this.mockMvc.perform(get("/").session(session))
+//                .andExpect(status().isFound())
+//                .andExpect(redirectedUrl("/search/mixed;keywords=spring,groovy"));
+//    }
+
+
+}
